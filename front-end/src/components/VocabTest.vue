da65ba04633795eb99ec351c551ad968cb5fd706
anhhong225 <anhhong225@gmail.com>
2024-12-01 17:24:37 +0700

    comment the vocabtest

--- front-end/src/components/VocabTest.vue
+++ front-end/src/components/VocabTest.vue
@@ -1,12 +1,11 @@
 <template>
-  <div>
+  <div><!--display score-->
     <h2>Score: {{ score }} out of {{ this.words.length }}</h2>
-
-    <form action="#" @submit.prevent="onSubmit">
+    <form action="#" @submit.prevent="onSubmit"> <!--a form for testing-->
       <div class="ui labeled input fluid">
         <div class="ui label">
           <i class="germany flag"></i> German
-        </div>
+        </div><!--read-only text for German-->
         <input type="text" readonly :disabled="testOver" :value="currWord.german"/>
       </div>
       <div class="ui labeled input fluid">
@@ -24,7 +23,7 @@
       <button class="positive ui button" :disabled="testOver">Submit</button>
     </form>
 
-    <p :class="['results', resultClass]">
+    <p :class="['results', resultClass]"><!--display result feedback-->
       <span v-html="result"></span>
     </p>
   </div>
@@ -40,39 +39,40 @@ export default {
     }
   },
   data() {
-    return {
+    return {//Randomly shuffle words for the test
       randWords: [...this.words.sort(() => 0.5 - Math.random())],
-      incorrectGuesses: [],
-      result: '',
-      resultClass: '',
-      english: '',
-      vietnamese: '',
+      incorrectGuesses: [],//Track incorrect guesses
+      result: '',//Store result feedback message
+      resultClass: '',//Store style for the result message
+      english: '', //Model for the English word input
+      vietnamese: '',//Model for the Vietnamese word input
       score: 0,
-      testOver: false
+      testOver: false //Flag indicating if the test is over
     };
   },
   computed: {
-    currWord: function() {
-      return this.randWords.length ? this.randWords[0] : '';
+    currWord: function() { //get the current word to check
+      return this.randWords.length ? this.randWords[0] : '';//Return the first word or an empty string if none left
     }
   },
   methods: {
-    onSubmit: function() {
+    onSubmit: function() {//Handle submission
+      // Check if the English and Vietnamese answer are correct
       if (this.english.toLowerCase() === this.currWord.english.toLowerCase() && this.vietnamese.toLowerCase() === this.currWord.vietnamese.toLowerCase()) {
         this.flash('Correct!', 'success', { timeout: 1000 });
         this.score += 1;
       } else {
         this.flash('Wrong!', 'error', { timeout: 1000 });
-        this.incorrectGuesses.push(this.currWord.german);
+        this.incorrectGuesses.push(this.currWord.german); //track incorrect answer
       }
-
+      //Reset input fields  
       this.english = '';
       this.vietnamese = '';
-      this.randWords.shift();
+      this.randWords.shift(); //remove tested word from array 
 
-      if (this.randWords.length === 0) {
-        this.testOver = true;
-        this.displayResults();
+      if (this.randWords.length === 0) { //check if all words are tested
+        this.testOver = true; //make flag test is over
+        this.displayResults(); //display result
       }
     },
     displayResults: function() {
